77f9b98744a71bb3fed4c71ae030a5c78cad296c
Dat <datguy7@gmail.com>
2019-02-14 22:35:33 -0500

    added relative link

--- js/script.js
+++ js/script.js
@@ -1,134 +1,5 @@
-// // $(function () {
-//   var scrolloffset = 55; //variable for menu height
 
-//   // When Scrollspy Detects a change
-//   $(window).on('activate.bs.scrollspy', function () {
-//     var hash = $('.site-nav')
-//       .find('a.active')
-//       .attr('href');
-
-//     if (hash !== '#page-hero') {
-//       $('header nav').addClass('inbody');
-//     } else {
-//       $('header nav').removeClass('inbody');
-//     }
-
-//     // Animate Media Layout when it passes scroll
-//     $('#page-media .animated-group').css(
-//       'visibility: hidden;'
-//     );
-
-//     if (hash === '#page-media') {
-//       $('#page-media .animated-group').addClass(
-//         'animated fadeInRight'
-//       );
-//     }
-//   });
-
-// $('#myModal').on('shown.bs.modal', function () {
-//   $('#myInput').trigger('focus')
-// })
-
-// Modifies modal and injects high resolution image
-// $('#site-modal').on('show.bs.modal', function(event) {
-//   $(this)
-//     .find('.modal-content img')
-//     .attr('src', $(event.relatedTarget).data('highres'));
-// });
-
-//   //Use smooth scrolling when clicking on navigation
-//   $('.navbar-nav a:not(.dropdown-toggle)').click(
-//     function () {
-//       if (
-//         location.pathname.replace(/^\//, '') ===
-//         this.pathname.replace(/^\//, '') &&
-//         location.hostname === this.hostname
-//       ) {
-//         var target = $(this.hash);
-//         target = target.length
-//           ? target
-//           : $('[name=' + this.hash.slice(1) + ']');
-//         if (target.length) {
-//           $('html,body').animate(
-//             {
-//               scrollTop: target.offset().top - scrolloffset
-//             },
-//             500
-//           );
-//           return false;
-//         } //target.length
-//       } //click function
-//     }
-//   ); //smooth scrolling
-// }); // Make sure Document loaded
-
-// Dat's Code --------------------------------------
-
-// Top Nav 
-
-
-// Side Nav hide/show
-// function toggle_class(className) {
-//   var elements = document.getElementsByClassName(className);
-
-//   if (elements[0].style.display === 'block') {
-//     for (var i = 0; i < elements.length; i++) {
-//       elements[i].style.display = 'none';
-//     }
-//   } else {
-//     for (var i = 0; i < elements.length; i++) {
-//       elements[i].style.display = 'block';
-//     }
-//   }
-// }
-
-
-// function toggle_id(idname) {
-//   var x = document.getElementById(idname);
-//   if (x.style.display === 'none') {
-//     x.style.display = 'block';
-//   } else {
-//     x.style.display = 'none';
-//   }
-// }
-
-
-
-
-// function button_click() {
-//   if ($(".fa-cube").hasClass(".active-teal")) {
-//     clicks += 1;
-//     document.getElementById("clicks").innerHTML = clicks;
-//   } else {
-//     clicks -= 1;
-//     document.getElementById("clicks").innerHTML = clicks;
-//   }
-
-// function addClick() {
-//   clicks += 1;
-//   document.getElementById("clicks").innerHTML = clicks;
-// }
-
-// function subClick() {
-//   clicks -= 1;
-//   document.getElementById("clicks").innerHTML = clicks;
-// }
-/*
-
-1. Check the page's address
-  window.location.href.indexOf(value)
-2. If the page's address matches one of the links
-
-3. It should be highlighted
-4. All others should not be highlighted
-
-*/
-// Tooltip Initiation BS4
-// $(".fa-cube").one("click", addBundle);
-/// 
-//
 $(function () {
-
   $(document).on('click', 'i.fa-times', function () {
     $(this).parents('li')
       .hide('slow', function () {
@@ -262,4 +133,142 @@ $(function () {
     $('#clicks').html(clicks);
   });
 
+  // var hrefbaseLocal = '<base href="C:/wamp64/www/Enfolink_py/" />';
+
+  // var hrefbaseServer = '<base href="http://localhost/Enfolink_py/" />';
+  // var links = "<a>add</a>";
+  // $('head').prepend(hrefbaseServer);
+
+
+
 });
+
+// // $(function () {
+//   var scrolloffset = 55; //variable for menu height
+
+//   // When Scrollspy Detects a change
+//   $(window).on('activate.bs.scrollspy', function () {
+//     var hash = $('.site-nav')
+//       .find('a.active')
+//       .attr('href');
+
+//     if (hash !== '#page-hero') {
+//       $('header nav').addClass('inbody');
+//     } else {
+//       $('header nav').removeClass('inbody');
+//     }
+
+//     // Animate Media Layout when it passes scroll
+//     $('#page-media .animated-group').css(
+//       'visibility: hidden;'
+//     );
+
+//     if (hash === '#page-media') {
+//       $('#page-media .animated-group').addClass(
+//         'animated fadeInRight'
+//       );
+//     }
+//   });
+
+// $('#myModal').on('shown.bs.modal', function () {
+//   $('#myInput').trigger('focus')
+// })
+
+// Modifies modal and injects high resolution image
+// $('#site-modal').on('show.bs.modal', function(event) {
+//   $(this)
+//     .find('.modal-content img')
+//     .attr('src', $(event.relatedTarget).data('highres'));
+// });
+
+//   //Use smooth scrolling when clicking on navigation
+//   $('.navbar-nav a:not(.dropdown-toggle)').click(
+//     function () {
+//       if (
+//         location.pathname.replace(/^\//, '') ===
+//         this.pathname.replace(/^\//, '') &&
+//         location.hostname === this.hostname
+//       ) {
+//         var target = $(this.hash);
+//         target = target.length
+//           ? target
+//           : $('[name=' + this.hash.slice(1) + ']');
+//         if (target.length) {
+//           $('html,body').animate(
+//             {
+//               scrollTop: target.offset().top - scrolloffset
+//             },
+//             500
+//           );
+//           return false;
+//         } //target.length
+//       } //click function
+//     }
+//   ); //smooth scrolling
+// }); // Make sure Document loaded
+
+// Dat's Code --------------------------------------
+
+// Top Nav 
+
+
+// Side Nav hide/show
+// function toggle_class(className) {
+//   var elements = document.getElementsByClassName(className);
+
+//   if (elements[0].style.display === 'block') {
+//     for (var i = 0; i < elements.length; i++) {
+//       elements[i].style.display = 'none';
+//     }
+//   } else {
+//     for (var i = 0; i < elements.length; i++) {
+//       elements[i].style.display = 'block';
+//     }
+//   }
+// }
+
+
+// function toggle_id(idname) {
+//   var x = document.getElementById(idname);
+//   if (x.style.display === 'none') {
+//     x.style.display = 'block';
+//   } else {
+//     x.style.display = 'none';
+//   }
+// }
+
+
+
+
+// function button_click() {
+//   if ($(".fa-cube").hasClass(".active-teal")) {
+//     clicks += 1;
+//     document.getElementById("clicks").innerHTML = clicks;
+//   } else {
+//     clicks -= 1;
+//     document.getElementById("clicks").innerHTML = clicks;
+//   }
+
+// function addClick() {
+//   clicks += 1;
+//   document.getElementById("clicks").innerHTML = clicks;
+// }
+
+// function subClick() {
+//   clicks -= 1;
+//   document.getElementById("clicks").innerHTML = clicks;
+// }
+/*
+
+1. Check the page's address
+  window.location.href.indexOf(value)
+2. If the page's address matches one of the links
+
+3. It should be highlighted
+4. All others should not be highlighted
+
+*/
+// Tooltip Initiation BS4
+// $(".fa-cube").one("click", addBundle);
+/// 
+//
